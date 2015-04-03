309edf21409ab86e674fe7d6c0a45642e333906b
chenzarf@gmail.com <chenzarf@gmail.com@a447eab3-0e9c-04ba-1c75-6003a4d6869d>
2015-04-03 09:24:42 +0000

    general planning finished

--- HW1/syscall_maxproc.h
+++ HW1/syscall_maxproc.h
@@ -16,18 +16,22 @@ int set_child_max_proc(int maxp){
 				//set max_proc to (father-1)
 			//else put -1
 		//set max proc to higher than father max proc -1
-		//
 	//update offspring field on PCB
 	//update max_proc field on PCB
 //tests
-	//
+	//check two/three generations max_proc field
+		//goal to verify our preserved env works (enough to check father value)
+	//negative parameter
+		//goal to check if max_proc values in path to root are not broken
 }
 
 int get_max_proc(){
-//
+//o(1) return the max_proc field.
 
 }
 
 int get_subproc_count(){
+//o(1) return the offspring field.
 
 }
+`
