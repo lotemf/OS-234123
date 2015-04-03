3d1df4f0033ae4c4f0f09bb4d090caf4ca78a0db
chenzarf <chenzarf@gmail.com>
2015-04-03 14:07:46 +0000

    created dummy implementations for the system calls

---
+++ HW1/kernel/syscall_maxproc.c
@@ -0,0 +1,17 @@
+#include <linux/kernel.h>
+#include <linux/sched.h>
+//TODO why did Uri include this header file here?
+//#include "count_sons.h" in our case its syscall_maxproc.h
+
+
+int sys_set_child_max_proc(int maxproc){
+	printk("in CR0, system call set_child_max_proc\n");
+}
+
+int sys_get_max_proc(){
+	printk("in CR0, system call get_max_proc\n");
+}
+
+int sys_get_subproc_count(){
+	printk("in CR0, system call get_subproc_count\n");
+}
