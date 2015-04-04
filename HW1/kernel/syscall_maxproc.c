f8fa1c88545c1b1091c331a6394a2ab77c700b70
chenzarf@gmail.com <chenzarf@gmail.com@a447eab3-0e9c-04ba-1c75-6003a4d6869d>
2015-04-04 16:31:23 +0000

    sys_set_child_max_proc function changed. updated state machine for all cases for this function. wasnt tested yet.

--- HW1/kernel/syscall_maxproc.c
+++ HW1/kernel/syscall_maxproc.c
@@ -1,5 +1,6 @@
 #include <linux/kernel.h>
 #include <linux/sched.h>
+#include "syscall_maxproc.h"
 //TODO why did Uri include this header file here?
 //#include "count_sons.h" in our case its syscall_maxproc.h
 
@@ -8,10 +9,51 @@ int sys_set_child_max_proc(int maxproc){
 	printk("in CR0, system call set_child_max_proc\n");
 	printk("maxproc promped value is:\t%d\n", maxproc);
 
-	struct task_struct *p;
+	struct task_struct *curr_proc;
+	struct task_struct *father_proc;
+	curr_proc = current;
+	father_proc = curr_proc->p_pptr;
 
-
-
+//if maxproc val is negative limit should be reset
+	//opt 1 - father has no limit - ok no limit
+	//opt 2 - father has a limit - ok father's limit minus 1
+	if (maxproc < 0){
+		if (father_proc->max_proc_num == -1){
+			curr_proc->max_proc_num = -1; // this is default value
+			return 0;
+		}
+		else {
+			curr_proc->max_proc_num = father_proc->max_proc_set - 1;
+			return 0;
+		}
+	}
+//if maxproc val is higher than father limit
+	//opt 1 - father has no limit - ok
+	//opt 2 - father has a limit - error
+	if (maxproc >= (father_proc->max_proc_num)-1){
+		if (father_proc->max_proc_num == -1){
+			curr_proc->max_proc_set = max_proc;
+			curr_proc->max_proc_num = max_proc;
+			return 0;
+		}
+		else{
+			errno = - EPERM;
+			return -1;
+		}
+	}
+//if father maxproc val is lower than the set value of son
+	if (father_proc->max_proc_num <= maxproc){
+		curr_proc->max_proc_num = father_proc->max_proc_num - 1;
+		curr_proc->max_proc_set = max_proc;
+		return 0;
+	}
+//if father maxproc val is not defined then just define ours
+	if ((father_proc->max_proc_num == -1) &&
+			(father_proc->max_proc_set == -1)){
+		curr_proc->max_proc_num = maxproc;
+		curr_proc->max_proc_set = maxproc;
+		return 0
+	}
 }
 
 int sys_get_max_proc(){
