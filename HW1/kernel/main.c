7cd1f0fd2004f9c3af9ee5ff1a3776d7260ab379
lotemf <lotemf@gmail.com>
2015-04-04 14:55:23 +0000

    #1.Changed Assembly Wrappers in syscall_maxproc.h - All is working #2.Changed form comments to running functions inside main for the tests

--- HW1/kernel/main.c
+++ HW1/kernel/main.c
@@ -5,10 +5,10 @@
 int main(){
 	printf("call to syscall 243, set child max proc\n");
 	set_child_max_proc(27);
-//	printf("call to syscall 244, get max proc\n");
-//	get_max_proc();
-//	printf("call to syscall 245, get subproc count\n");
-//	get_subproc_count();
+	printf("call to syscall 244, get max proc\n");
+	get_max_proc();
+	printf("call to syscall 245, get subproc count\n");
+	get_subproc_count();
 
 
 	return 0;
