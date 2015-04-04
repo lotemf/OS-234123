98af40eeefc054a2f394d0ec4228423a0b1666be
lotemf@gmail.com <lotemf@gmail.com@a447eab3-0e9c-04ba-1c75-6003a4d6869d>
2015-04-04 14:55:23 +0000

    #1.Changed Assembly Wrappers in syscall_maxproc.h - All is working #2.Changed form comments to running functions inside main for the tests

--- HW1/kernel/syscall_maxproc.h
+++ HW1/kernel/syscall_maxproc.h
@@ -31,7 +31,7 @@ int set_child_max_proc(int maxp){
 	long __res;
 	__asm__ volatile (
 	"movl $243, %%eax;"
-	/*Needs Verification*/"movl %1, %%ebx;"						/*Moving the int value of maxp -> maybe there is a need to edit this part*/
+	"movl %1, %%ebx;"						/*Moving the int value of maxp -> maybe there is a need to edit this part*/
 	"int $0x80;"												/*because the size of the registers might not match*/
 	"movl %%eax,%0"
 	: "=m" (__res)
@@ -49,38 +49,40 @@ int set_child_max_proc(int maxp){
  * get_max_proc() - Returns the max_proc field of the running process.
  * Complexity- o(1)
  ******************************************************************************/
-/*int get_max_proc(){
+int get_max_proc(){
 	long __res;
 	__asm__ volatile (
 	"movl $244, %%eax;"
 	"int $0x80;"
 	"movl %%eax,%0"
 	: "=m" (__res)
+	:
 	: "%eax"
 	);
 	if ((unsigned long)(__res) >= (unsigned long)(-125)) {
-	errno = -(__res); __res = -1;	*/						/*This code takes the error value */
-	/*}		*/												/*and stores it inside errno by using the unsigned*/
-	/*return (int)(__res);	*/								/*value returned from the get_max_proc func*/
+	errno = -(__res); __res = -1;							/*This code takes the error value */
+	}														/*and stores it inside errno by using the unsigned*/
+	return (int)(__res);									/*value returned from the get_max_proc func*/
 
-/*}*/
+}
 
 /*******************************************************************************
  * get_max_proc() - Returns the offspring field of the running process.
  * Complexity- o(1)
  ******************************************************************************/
-/*int get_subproc_count(){
+int get_subproc_count(){
 	long __res;
 	__asm__ volatile (
 	"movl $245, %%eax;"
 	"int $0x80;"
 	"movl %%eax,%0"
 	: "=m" (__res)
+	:
 	: "%eax"
 	);
 	if ((unsigned long)(__res) >= (unsigned long)(-125)) {
 	errno = -(__res); __res = -1;
 	}
 	return (int)(__res);
-}*/
+}
 
