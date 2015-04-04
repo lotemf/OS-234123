3c923b0dd327b4becbbb85fc8ecbf6782c25d4ff
chenzarf@gmail.com <chenzarf@gmail.com@a447eab3-0e9c-04ba-1c75-6003a4d6869d>
2015-04-04 15:22:21 +0000

    minor changes

--- HW1/kernel/syscall_maxproc.c
+++ HW1/kernel/syscall_maxproc.c
@@ -7,6 +7,11 @@
 int sys_set_child_max_proc(int maxproc){
 	printk("in CR0, system call set_child_max_proc\n");
 	printk("maxproc promped value is:\t%d\n", maxproc);
+
+	struct task_struct *p;
+
+
+
 }
 
 int sys_get_max_proc(){
