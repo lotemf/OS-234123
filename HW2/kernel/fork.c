957577a9bf2f0fd2d820d08419a8d61a102bfb71
alon.vizel <alon.vizel@a447eab3-0e9c-04ba-1c75-6003a4d6869d>
2015-05-01 22:55:31 +0000

    moved the enqueue and dequeue of the father in fork inside the function wake_up_forked_process in sched.c

--- HW2/kernel/fork.c
+++ HW2/kernel/fork.c
@@ -751,10 +751,6 @@ int do_fork(unsigned long clone_flags, unsigned long stack_start,
 			else {
 				p->requested_trials = 1;
 				--current->requested_trials; //now the father is overdue
-				dequeue_task(current, rq->SHORT);
-				//need to check if this action happen somewhere else and change this
-				current->prio = MAX_PRIO;
-				enqueue_task(current, rq->SHORT_OVERDUE);
 			}
 			p->used_trials = 0;
 		}
