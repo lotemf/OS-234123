03b62b775897370ead5a46feaf983ce87dd29703
alon.vizel <alon.vizel@a447eab3-0e9c-04ba-1c75-6003a4d6869d>
2015-05-01 22:55:31 +0000

    moved the enqueue and dequeue of the father in fork inside the function wake_up_forked_process in sched.c

--- HW2/kernel/sched.c
+++ HW2/kernel/sched.c
@@ -509,6 +509,14 @@ void wake_up_forked_process(task_t * p)
 		p->sleep_avg = p->sleep_avg * CHILD_PENALTY / 100;
 		p->prio = effective_prio(p);
 	}
+	if(IS_OVERDUE(current)){
+		dequeue_task(current, rq->SHORT);
+		current->prio = MAX_PRIO;
+		enqueue_task(current, rq->SHORT_OVERDUE);
+	} else if (IS_SHORT(current)) {
+		dequeue_task(current, rq->SHORT);
+		enqueue_task(current, rq->SHORT);
+	}
 	p->cpu = smp_processor_id();
 	activate_task(p, rq);
 
