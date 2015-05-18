1c7a9f30720fd346e409b2c2d4581734b2be31ac
chenzarf <chenzarf@gmail.com>
2015-05-18 13:54:34 +0000

    tpCreate impl was added and ThreadPool struct was changed
    still to do:
    -refactor code of null check in tpCreate
    -change startThreadRoutine function

--- HW3/threadPool.h
+++ HW3/threadPool.h
@@ -1,13 +1,41 @@
 #ifndef __THREAD_POOL__
 #define __THREAD_POOL__
 
+/*
+ * Includes
+ */
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include "osqueue.h"
+
+/*
+ * Structs  and Enums
+ */
 typedef struct thread_pool
 {
- //The field x is here because a struct without fields
- //doesn't compile. Remove it once you add fields of your own
- int x;
- //TODO - FILL THIS WITH YOUR FIELDS
+	int numOfThreads;
+	OsQueue* tasksQueue;
+	pthread_t* threadsArrPtr;
+	sem_t* semaphore;
+	pthread_mutex_t* tasksMutex;
+	bool destroyFlag;
+ //semaphur
+ //number of threads
+ //pointer to queue of tasks
+ //array of pointers to thread ids
+
 }ThreadPool;
+
+/*
+ * Thread Pool Public Functions
+ */
+
+//step 1: create an array of pthread_t
+//step 2: create all threads
+//step 3: create the semaphore
+//step 3: create the task queue
+//TODO create destroy functions for each field in struct.
 
 ThreadPool* tpCreate(int numOfThreads);
 
