d562d5df28787591ba0863f324c59da094a10317
lotemf <lotemf@gmail.com>
2015-06-09 17:16:12 +0000

    There is still a missing concept for write, but I have the idea.... I just need to figure out the implementation (tomorrow) The read func should be changed using alon's Code - Tomorrow

--- HW4/snake.c
+++ HW4/snake.c
@@ -1,7 +1,8 @@
 #include <linux/errno.h>
 #include <linux/module.h>
 #include <linux/fs.h>
-
+#include <linux/slab.h>         // for using the kmalloc function
+#include <linux/spinlock.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>
 
@@ -13,30 +14,37 @@
 #include "snake.h"
 MODULE_LICENSE("GPL");
 
-#define WHITE 1						//for the color checks
-#define BLACK 2
+#define SNAKE_ERROR -1
+#define WHITE_COLOR 4						//for the color checks
+#define BLACK_COLOR 2
 /*******************************************************************************
 		Variables to be used in the implementation of the new module
 *******************************************************************************/
 static int major = -1;
 struct file_operations fops;
 int maxGames = 0;
-
 int* players_num;
+int* is_played;							//A flag that notes if the game started or not
+int* black_player_pid;
+int* white_player_pid;	//Every game hold the pids of the players (of the two processes)
+
+//Locks
+struct semaphore* game_sema;
 spinlock_t* players_lock;				//The spinlock is used to check the legal number of players for each device (which is 2 players)
 
 //Wait queue for the device, so if one of the players is playing the other one is in the wait queue
 wait_queue_head_t* wait_queue;
 
 
-
 MODULE_PARM(maxGames,"i");	//This is the only input needed for the module
 							//But still it is unclear how we receive this param
 							//(maybe in the makefile...)
 
+
+
 typedef struct {
-    int minor;			//In the char Device based on this model
-	int max_games;		//every device has it's own maxGames amount and minor (maybe more fields...)
+    int minor;				//In the char Device based on this model
+//	int max_games;			//every device has it's own maxGames amount and minor (maybe more fields...)
 } dev_private_data;
 
 /******************************************************************************/
@@ -44,17 +52,27 @@ typedef struct {
 
 
 /*******************************************************************************
- * snake_open - The implementation is as shown in the video tutorial (20) by
-  	  	  	    Leonid, with an addition - a check of the amount of players
-  	  	  	    permitted for this device (2)
+ * snake_open - This function opens a new game on the Snake device.
+ 	 	 	 	The process that opens the device will get an automatic
+ 	 	 	 	white color if it's the first process, or automatically a black
+ 	 	 	 	color if it's the second process.
 
-  	  	  	    **Notice - Not finished yet, the "Under Construction" part
-  	  	  	    		   of the code is in pseudo-code as comment inside
-  	  	  	    		   the spinlock...
+ 	 	 	 	If a process is the first to call open on this device
+ 	 	 	 	for the game, it should wait on the semaphore until another
+ 	 	 	 	process calls open, and then it's GAME ON.
+
+
+ 	 	 	 	P.S. - The way to contact this specific game for the two processes
+ 	 	 	 	that are playing in it, will be through the Minor num when
+ 	 	 	 	"talking" to the device driver.
 *******************************************************************************/
 int snake_open(struct inode* inode, struct file* fileptr){
 	fileptr->f_op = &fops;
     int minor = MINOR(inode->i_rdev);										//Extracting the minor from the device
+
+    if (players_num[minor] == 2){
+    	return -EPERM;						//We check if this game is full...
+    }
 
     fileptr->private_data = kmalloc(sizeof(dev_private_data), GFP_KERNEL);	//Allocating the memory for the struct using a kernel alloc
     if (!(fileptr->private_data)){
@@ -63,49 +81,71 @@ int snake_open(struct inode* inode, struct file* fileptr){
 
     dev_private_data* dev_p_data = (device_private_data*)(filp->private_data);
     dev_p_data->minor = minor;
-    dev_p_data->max_games = maxGames;
+//    dev_p_data->max_games = maxGames;
 
     spin_lock(players_lock[minor]);
-    if (!players_num[minor]){		//This means this player should get the
+    if (!players_num[minor]){		//This means this player should get the white color
     	players_num[minor]++;
-    	//set white color for this player
-    	//block the usage of this device until black player arrives
-    	//maybe using a semaphore...
+    	white_player_pid[minor]=getpid();				//set white color for this player
+    	//** make this device wait on the semaphore
     } else if (players_num[minor] == 1){
-    	players_num++;
-    	//set black color for this player
-    	//unblock the usage of this device
-    	//maybe using a semaphore...
-    } else {
-    	return -EPERM;		//because there are already 2 players, so it's not permitted
+    	players_num[minor]++;
+    	dev_p_data->black_player_pid=getpid();				//set black color for this player
+    	//** make this device free the semaphore (using signal or up or whatever...)
+    	up(&game_sema[minor]);
+    	return 0;					//We return from the function because we wouldn't like to make another wait on the semaphore
     }
     spin_unlock(players_lock[minor]);
 
+    down_interruptible(&game_sema[minor]);					//We wait on the semaphore
 	return 0;
 }
 /*******************************************************************************
- * snake_release -
+ * snake_release - Freeing the allocated private data, and reducing the amount
+ 	 	 	 	   of players in the game so when we check the write/read
+ 	 	 	 	   values we know if it's a legal call or not
 *******************************************************************************/
 int snake_release(struct inode* inode, struct file* filp){
 
-	kfree(filp->private_data);
-	int minor = get_minor_from_inode(inode);
+	int minor = MINOR(inode->i_rdev);
+
 	//Update counters:
 	spin_lock(players_lock[minor]);
 	players_num[minor]--;
-	wake_up_interruptible(&wait_queue[minor]);
 		//Maybe we need to add some more lines here after the changes in snake_open
 	spin_unlock(players_lock[minor]);
+
+	kfree(filp->private_data);
+
 	return 0;
 }
 /*******************************************************************************
- * snake_read -
+ * snake_read - This function prints the board, using the buffer supplied.
+ 	 	 	 	If a partial print was performed, we need to edit the buffer
+ 	 	 	 	so the output of it will be a string (add "/0");
+
+
 *******************************************************************************/
 ssize_t snake_read(struct file* filp, char* buffer, size_t count, loff_t* f_pos){	//This function should be planned carefully
+	//Checking it's a legal game
+	int minor = ((dev_private_data *)((filp)->private_data))->minor;
+
+	if ( (players_num[minor] == 2) && (is_played[minor]) ){		//This means it's a legit game and we can do a read
+		//Use alon's Function to print the board but handle the synchronization so the second player couldn't write to the board
+		//Make him change the print function in the hw3q1 so it will return a erady buffer
+
+		char* new_buffer;
+		//Get the length of the function using strlen
+		//and then use copy to user...
+	}
 	return 0;
 }
 /*******************************************************************************
- *  snake_write -
+ *  snake_write - This is the complex part because we need to synchronize
+ 	 	 	 	  the writes between the two processes using  new semaphore
+ 	 	 	 	  maybe we will call it write_sema...
+ 	 	 	 	  and we might need a new spinlock for the update of the
+ 	 	 	 	  write...  //Talk to chen about it
 *******************************************************************************/
 ssize_t snake_write(struct file* filp, const char* buffer, size_t count, loff_t* f_pos){	//This function should be planned carefully
 	return 0;
@@ -121,15 +161,23 @@ loff_t snake_llseek(struct file* filp, loff_t irrelevant, int num){
  * snake_ioctl - Control Commands API according to the supplied header file
 *******************************************************************************/
 int snake_ioctl(struct inode* inode, struct file* filp, unsigned int command, unsigned long var){
-    switch(command) {
+	int minor = MINOR(inode->i_rdev);
+
+	switch(command) {
             case SNAKE_GET_WINNER:
-				//We still need to understand how to return the winner
+				//We still need to understand how to return the winner using the hw3q1.c prog
             	//maybe we need to make a new field in the private data struct
                 break;
             case SNAKE_GET_COLOR:
-				//We still need to understand how to return the current player's color
+				if (getpid() == black_player_pid[minor]){
+					return BLACK_COLOR;
+				} else if (getpid() == white_player_pid[minor]){
+					return WHITE_COLOR;
+				} else {
+					return -ENOTTY;
+				}
             	//maybe we need to make a new field in the private data struct
-            	break;
+            break;
             default: return -ENOTTY;
     }
     return 0;
@@ -159,15 +207,25 @@ int init_module(void){
 
 	//I think we need to allocate all of the memory for the vars manually
 	//using kmallock because we don't know how many instances we are going to have of the same device
+    players_lock = kmalloc(sizeof(spinlock_t)*maxGames, GFP_KERNEL);		//spinlocks array
+    game_sema = kmalloc(sizeof(struct semaphore)*maxGames, GFP_KERNEL);		//Not sure about the syntax here //semaphores array
+    players_num = kmalloc(sizeof(int)*maxGames, GFP_KERNEL);
+    is_played = kmalloc(sizeof(int)*maxGames, GFP_KERNEL);
+    white_player_pid = kmalloc(sizeof(int)*maxGames, GFP_KERNEL);
+    black_player_pid = kmalloc(sizeof(int)*maxGames, GFP_KERNEL);
 
 	//   -- Should we allocate each array as the size of the maxGames we get in the input?
 
 	int i;
 	for (i=0;i<maxGames;i++){
 		spin_lock_init(&players_lock[i]);
+		is_played[i] = 0;
 		players_num[i] = 0;
+		black_player_pid[i] = -1;
+		white_player_pid[i] = -1;
+        sema_init(&game_sema[i], 1);
 
-		//still need to add some more variables here, and decide about the malloc
+		//still need to add some more variables here
 	}
 
 	/*Test*/printk("Hello, World!\n");
@@ -181,6 +239,14 @@ void cleanup_module(void){
     if (retval < 0){															//HW4 - Lotem - Finished...
     	printk("cleanup_module -> Failed unregistering character device\n");
     }
+
+    //Freeing all of the arrays
+    kfree(players_lock);
+    kfree(game_sema);
+    kfree(players_num);
+    kfree(is_played);
+    kfree(black_player_pid);
+    kfree(white_player_pid);
 
     /*Test*/printk("Goodbye, Cruel World!\n");
     return;
