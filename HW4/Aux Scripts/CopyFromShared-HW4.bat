20501477b475fca42544a31514a717fe26453b1d
chenzarf <chenzarf@gmail.com>
2015-06-18 12:40:37 +0000

    everything.

--- HW4/Aux Scripts/CopyFromShared-HW4.bat
+++ HW4/Aux Scripts/CopyFromShared-HW4.bat
@@ -2,6 +2,6 @@
 echo Copying
 
 cd /root/tests/HW4
-cp /mnt/hgfs/SharedFolder/Not_In_Kernel/* ./ -r
+cp /mnt/hgfs/vm_shared_folder/HW4/Not_In_Kernel/* ./ -r
 
 $SHELL
--- HW4/Aux Scripts/copyToSharedHW4.bat
+++ HW4/Aux Scripts/copyToSharedHW4.bat
@@ -1,2 +1,2 @@
-cd D:\OSTech\SharedFolder
-xcopy C:\Users\Lotem\workspace\svn\HW4\* Not_In_Kernel /S /Y
+cd C:\Work\VMs\vm_shared_folder\HW4
+xcopy C:\Work\Workspaces\CCppWorkspace\svn\HW4\* Not_In_Kernel /S /Y
